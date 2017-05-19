e131a971272ed73f9b48716bbf99a59093958456
min_xu <xumin_wlt@163.com>
2017-05-19 10:17:48 +0800

    add server class support

---
+++ src/main/java/me/j360/rpc/server/RPCServerOption.java
@@ -0,0 +1,10 @@
+package me.j360.rpc.server;
+
+/**
+ * Package: me.j360.rpc.server
+ * User: min_xu
+ * Date: 2017/5/19 上午10:16
+ * 说明：
+ */
+public class RPCServerOption {
+}
---
+++ src/main/java/me/j360/rpc/server/handler/RPCServerHandler.java
@@ -0,0 +1,10 @@
+package me.j360.rpc.server.handler;
+
+/**
+ * Package: me.j360.rpc.server.handler
+ * User: min_xu
+ * Date: 2017/5/19 上午10:17
+ * 说明：
+ */
+public class RPCServerHandler {
+}
