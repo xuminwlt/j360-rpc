207e6926ad0c14f3111da595a1005a91a899db9e
min_xu <xumin_wlt@163.com>
2017-05-23 15:41:29 +0800

    add spring support

---
+++ src/main/java/me/j360/rpc/spring/RPCClientFactoryBean.java
@@ -0,0 +1,10 @@
+package me.j360.rpc.spring;
+
+/**
+ * Package: me.j360.rpc.spring
+ * User: min_xu
+ * Date: 2017/5/23 下午3:40
+ * 说明：
+ */
+public class RPCClientFactoryBean {
+}
---
+++ src/test/java/me/j360/rpc/spring/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package: me.j360.rpc.spring
+ * User: min_xu
+ * Date: 2017/5/23 下午3:40
+ * 说明：
+ */
+package me.j360.rpc.spring;
