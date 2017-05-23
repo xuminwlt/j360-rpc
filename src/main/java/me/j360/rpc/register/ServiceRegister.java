03f946d0f5169e6b83a19828f89f521163b018ca
min_xu <xumin_wlt@163.com>
2017-05-23 18:44:21 +0800

    add zk register support

--- src/main/java/me/j360/rpc/register/ServiceRegister.java
+++ src/main/java/me/j360/rpc/register/ServiceRegister.java
@@ -1,5 +1,11 @@
 package me.j360.rpc.register;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Package: me.j360.rpc.register
  * User: min_xu
@@ -9,14 +15,46 @@ package me.j360.rpc.register;
 public class ServiceRegister {
 
 
+    private CountDownLatch latch = new CountDownLatch(1);
+
     private String zkAddress;
+
+    private static CuratorFramework client;
 
     public ServiceRegister(String zkAddress) {
         this.zkAddress = zkAddress;
     }
 
+    private void init() {
+
+    }
+
 
     public void register(String data) {
+        if (data != null) {
+            client = getClient();
+            if (client != null) {
 
+            }
+        }
+    }
+
+
+    private void createNode(String data) {
+
+    }
+
+    private CuratorFramework getClient() {
+        try {
+            client =  CuratorFrameworkFactory.builder().connectString(zkAddress)
+                    .retryPolicy(new RetryNTimes(RETRY_TIME, RETRY_INTERVAL))
+                    .connectionTimeoutMs(CONNECT_TIMEOUT).build();
+
+            return client;
+            latch.countDown();
+        } catch (Exception e) {
+
+        }
+        return null;
     }
 }
